cd4a7f44bc43ac154613635461a1fec6b0cf90b3
Bhagirathsinh Rathod <wizard.tech.54@gmail.com>
2021-06-30 19:53:34 +0530

    Update 30 June 4

---
+++ main.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Moonlight Event Planners </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="mainstyle.css" type="text/css">
+    </head>
+    <body>
+    <div class="nav"> Link</div>
+      <div class="container">
+        <section class="section home">
+          <div class="image1"> <img src="images/logo4.svg"> </div>
+          <div class="para1"> 
+            <h1> Managing Events Is Easier Now </h1> 
+            <p> Moontimes has incredible features that makes organizing and managing events easier. </p>
+          </div>
+        </section>
+        <section class="section About">
+          <div class="para2">
+            <h1> About Our App </h1> 
+            <p> Moontime is a event management application. You can organize and manage youe events.
+              Managing events with Moontime gets easy as you get a tonnes of features. Some of are featues includes
+              <ul>
+                <li> Minimal Design </li>
+                <li> Create new events </li>
+                <li> Update status of event </li>
+                <li> Delete outdated events </li>
+              </ul>
+            </p>
+          </div>
+        </section>
+        <section class="section reviews">
+          <h1>Customer Reviews</h1>
+          
+        </section>
+        <section class="section">
+          <h3>Page 4</h3>
+        </section>
+      </div>
+    </body>
+</html>
