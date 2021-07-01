1547b0a8c07ebec1edd481f528e94ebe50622b11
Bhagirathsinh Rathod <wizard.tech.54@gmail.com>
2021-07-01 07:57:49 +0530

    Update 1st July 1

--- main.php
+++ main.php
@@ -3,23 +3,38 @@
     <head>
         <title> Moonlight Event Planners </title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="mainstyle.css" type="text/css">
+        <link rel="stylesheet" href="main_style.css" type="text/css">
+        <script src="https://kit.fontawesome.com/2fef570697.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="docSlider.css">
     </head>
     <body>
-    <div class="nav"> Link</div>
-      <div class="container">
-        <section class="section home">
+      <div class="container docSlider">
+        <section id="home" class="section inner">
+        <div class="nav">
+          <ul>
+            <li><a href="#home"> Home </a></li>
+            <li><a href="#about"> About </a></li>
+            <li><a href="#review"> Reviews </a></li>
+            <li><a href="#pricing"> Pricing </a></li>
+            <li><a href="#contact"> Contact </a></li>
+            <li><a href="login.php"> Login </a></li>
+          </ul>
+        </div>
           <div class="image1"> <img src="images/logo4.svg"> </div>
           <div class="para1"> 
             <h1> Managing Events Is Easier Now </h1> 
             <p> Moontimes has incredible features that makes organizing and managing events easier. </p>
+            <p id="wheel"> Scroll <a href="#about"><i class="fas fa-long-arrow-alt-right"></i></a></p>
           </div>
         </section>
-        <section class="section About">
+        <section id="about" class="section inner">
+          <div class="image2">
+            <img src="images/img1.svg">
+          </div>
           <div class="para2">
-            <h1> About Our App </h1> 
-            <p> Moontime is a event management application. You can organize and manage youe events.
-              Managing events with Moontime gets easy as you get a tonnes of features. Some of are featues includes
+            <h1> About Moontimes </h1> 
+            <p> Moontimes is a event management application. You can organize and manage youe events.
+              Managing events with Moontimes gets easy as you get tonnes of features. Some of are featues includes
               <ul>
                 <li> Minimal Design </li>
                 <li> Create new events </li>
@@ -29,13 +44,17 @@
             </p>
           </div>
         </section>
-        <section class="section reviews">
-          <h1>Customer Reviews</h1>
-          
+        <section class="section inner" id="review">
+          <div class="review1"> hello </div>
+          <div class="review2"> hello </div>
+          <div class="review3"> hello </div>
+          <div class="heading"> hello </div>
         </section>
-        <section class="section">
+        <section class="section innner">
           <h3>Page 4</h3>
         </section>
       </div>
+      <script src="docSlider.min.js"></script>
+      <script>docSlider.init({horizontal: true});</script>
     </body>
 </html>
